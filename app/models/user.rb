40bc2fd76d5379b0d030dc5280344104ca310401
DerekBaert <derek.baert@gmail.com>
2022-04-06 14:48:10 -0500

    Implemented devise log in and authentification

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
-
-    validates :name, :email, :password, :address, :phone_number, presence: true
-    validates :email, uniqueness: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,28 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :games
   resources :orders
-  resources :users
+  # resources :users
   resources :genres
   resources :platforms
 
   root to: 'home#index'
 
-  get    'login'   => 'sessions#new'
-  get    'logout'  => 'sessions#destroy'
-  get    'signup'  => 'users#new'
-  get    'account' => 'users#show'
+  # get    'login'   => 'sessions#new'
+  # get    'logout'  => 'sessions#destroy'
+  # get    'signup'  => 'users#new'
+  # get    'account' => 'users#show'
   get    'home'    => 'home#index'
   get    'search'  => 'search#index'
 
-  post   'login'   => 'sessions#create' 
-  delete 'logout'  => 'sessions#destroy'
+  # post   'login'   => 'sessions#create' 
+  # delete 'logout'  => 'sessions#destroy'
 
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
