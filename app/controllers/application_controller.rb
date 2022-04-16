38b398e41adb4bc2b94e80b2189b5bd278e05105
DerekBaert <derek.baert@gmail.com>
2022-04-15 19:41:03 -0500

    Updated active_admin to handle province

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,26 +33,52 @@ class ApplicationController < ActionController::Base
         session[:cart_platforms].each do |k, v|
             @cart_count += v
         end
+
+        if(@cart_count != 0)
+            calculate_total()
+        end        
     end
     
+    def calculate_total
+        @sub = 0
+        @tax_rate = 0
+
+        if(user_signed_in?)
+            province = current_user.province
+            @tax_rate += province.gst unless province.gst.nil?
+            @tax_rate += province.pst unless province.pst.nil?
+            @tax_rate += province.hst unless province.hst.nil?
+        end
+
+        @cart.each do |p|
+            @sub += p.price
+        end
+        @tax = @sub * @tax_rate
+        @total = @sub + @tax
+    end
+
     def remove_game_from_cart
         session[:cart_games].delete(params[:id])
-        redirect_to request.referrer  
+        load_cart()
+        redirect_to cart_path  
     end
 
     def remove_platform_from_cart
         session[:cart_platforms].delete(params[:id])
-        redirect_to request.referrer  
+        load_cart()
+        redirect_to cart_path 
     end
 
     def update_game_quantity
         session[:cart_games][params[:id]] = params[:quantity].to_i
-        redirect_to request.referrer 
+        load_cart()
+        redirect_to cart_path 
     end
 
     def update_platform_quantity
         session[:cart_platforms][params[:id]] = params[:quantity].to_i
-        redirect_to request.referrer 
+        load_cart()
+        redirect_to cart_path
     end
     
     private
