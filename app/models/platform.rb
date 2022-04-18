98d544f49a5240dd3ca8c45df3c465a5fbcc691d
DerekBaert <derek.baert@gmail.com>
2022-04-18 15:35:13 -0500

    Update platform.rb

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -2,6 +2,9 @@ class Platform < ApplicationRecord
     has_many :game_platforms, dependent: :delete_all
     has_many :Games, through: :game_platforms
 
+    has_many :platform_orders, dependent: :delete_all
+    has_many :orders, through: :platform_orders
+
     has_one_attached :image
 
     validates :name, :price, :description, presence: true
